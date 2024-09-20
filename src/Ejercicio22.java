dd369a8e9687a78c5e1300bdca98e5cd7128902e
samadesan <samadesan@alu.edu.gva.es>
2024-09-20 11:47:43 +0200

    Prueba

---
+++ src/Ejercicio22.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Ejercicio22 {
+    public static void main(String[] args) {
+        int num;
+        int positivos = 0;
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
+        for (int i = 0; i <= 10; i++) {
+            num = sc.nextInt();
+            if (num > 0) {
+                positivos ++;
+            }
+        }
+        System.out.println("Positivos: " + positivos);
+    }
+}
+
